4fde56561b760a9f047e2901bddcea49a678a731
Danny D <dddominguez@utpl.edu.ec>
2022-07-29 17:57:24 -0400

    config para envio de emails

---
+++ app/Mail/RegistroSolicitud.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RegistroSolicitud extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $emailData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->emailData = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('jocdominguez@gmail.com', 'Pet app')
+        ->subject('Notificación Prevención abandono animal')
+        ->view('mails.request_create');
+    }
+}
